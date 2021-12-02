fde1208bd6ee8385ac522c051ad5cdc36739a0af
tylerturk <tturktni@gmail.com>
2021-12-01 23:11:16 -0600

    Solved day 2

--- 2021/day02/src/main.rs
+++ 2021/day02/src/main.rs
@@ -1,14 +1,40 @@
 fn main() {
     println!("Part 1: {}", solve_part_1(aoc::input()));
-    println!("Part 2: {}", solve_part_1(aoc::input()));
+    println!("Part 2: {}", solve_part_2(aoc::input()));
 }
 
+// matrix
+// forward increases x
+// up decreases y
+// down increases y
 fn solve_part_1(contents: String) -> i32 {
-    todo!();
+    let mut pos: (i32, i32) = (0, 0);
+    for line in contents.lines() {
+        let mut pieces = line.split(" ");
+        let command = pieces.next().unwrap();
+        let modifier = pieces.next().unwrap().parse::<i32>().unwrap();
+        pos = match command {
+            "forward" => (pos.0 + modifier, pos.1),
+            "up" => (pos.0, pos.1 - modifier),
+            _ => (pos.0, pos.1 + modifier),
+        };
+    }
+    pos.0 * pos.1
 }
 
 fn solve_part_2(contents: String) -> i32 {
-    todo!();
+    let mut pos: (i32, i32, i32) = (0, 0, 0);
+    for line in contents.lines() {
+        let mut pieces = line.split(" ");
+        let command = pieces.next().unwrap();
+        let modifier = pieces.next().unwrap().parse::<i32>().unwrap();
+        pos = match command {
+            "forward" => (pos.0 + modifier, pos.1 + modifier * pos.2, pos.2),
+            "up" => (pos.0, pos.1, pos.2 - modifier),
+            _ => (pos.0, pos.1, pos.2 + modifier),
+        };
+    }
+    pos.0 * pos.1
 }
 
 #[cfg(test)]
@@ -17,11 +43,11 @@ mod tests {
 
     #[test]
     fn part_1() {
-        assert_eq!(solve_part_1(aoc::sample()), 7);
+        assert_eq!(solve_part_1(aoc::sample()), 150);
     }
 
     #[test]
     fn part_2() {
-        assert_eq!(solve_part_2(aoc::sample()), 5);
+        assert_eq!(solve_part_2(aoc::sample()), 900);
     }
 }
--- 2021/template/src/main.rs
+++ 2021/template/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Part 1: {}", solve_part_1(aoc::input()));
-    println!("Part 2: {}", solve_part_1(aoc::input()));
+    println!("Part 2: {}", solve_part_2(aoc::input()));
 }
 
 fn solve_part_1(contents: String) -> i32 {
@@ -17,11 +17,11 @@ mod tests {
 
     #[test]
     fn part_1() {
-        assert_eq!(solve_part_1(aoc::sample()), 7);
+        assert_eq!(solve_part_1(aoc::sample()), 0);
     }
 
     #[test]
     fn part_2() {
-        assert_eq!(solve_part_2(aoc::sample()), 5);
+        assert_eq!(solve_part_2(aoc::sample()), 0);
     }
 }
