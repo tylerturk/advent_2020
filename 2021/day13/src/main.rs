381677ebf26b6c5ab8e21e43769d45223cc5253b
tylerturk <tturktni@gmail.com>
2021-12-13 20:41:06 -0600

    Update

--- 2021/day13/src/main.rs
+++ 2021/day13/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     solve_part_2(aoc::input());
 }
 
-fn solve_part_1(contents: String) -> usize {
+fn fold_paper(contents: String, one_fold: bool) -> HashMap<String, bool> {
     let mut papers: HashMap<String, bool> = HashMap::new();
     let mut coords = true;
     let mut folds: Vec<String> = Vec::new();
@@ -60,68 +60,23 @@ fn solve_part_1(contents: String) -> usize {
                 }
             }
         }
-        break;
+        if one_fold {
+            break;
+        }
     }
+    papers
+}
+
+fn solve_part_1(contents: String) -> usize {
+    let papers = fold_paper(contents, true);
     papers.keys().len()
 }
 
 fn solve_part_2(contents: String) {
-    let mut papers: HashMap<String, bool> = HashMap::new();
-    let mut coords = true;
-    let mut folds: Vec<String> = Vec::new();
-    for line in contents.lines() {
-        if line.is_empty() {
-            coords = false;
-            continue;
-        }
-        if coords {
-            papers.insert(line.to_string(), true);
-        } else {
-            let fold: String = line
-                .split_whitespace()
-                .collect::<Vec<&str>>()
-                .iter()
-                .last()
-                .unwrap()
-                .to_string();
-            folds.push(fold);
-        }
-    }
+    let papers = fold_paper(contents, false);
+    let mut output: Vec<Vec<bool>> = Vec::new();
 
-    for fold in folds {
-        let fold_bits: Vec<&str> = fold.split("=").collect();
-        let fold_pos: usize = fold_bits.last().unwrap().parse::<usize>().unwrap();
-        if fold_bits.first().unwrap() == &"x" {
-            for key in papers.clone().keys() {
-                let pos_bits: Vec<&str> = key.split(",").collect();
-                let x = pos_bits.first().unwrap().parse::<usize>().unwrap();
-                let y = pos_bits.last().unwrap().parse::<usize>().unwrap();
-                if x == fold_pos {
-                    papers.remove(key);
-                } else if x > fold_pos {
-                    let new_x = 2 * fold_pos - x;
-                    papers.insert(format!("{},{}", new_x, y), true);
-                    papers.remove(key);
-                }
-            }
-        } else {
-            for key in papers.clone().keys() {
-                let pos_bits: Vec<&str> = key.split(",").collect();
-                let x = pos_bits.first().unwrap().parse::<usize>().unwrap();
-                let y = pos_bits.last().unwrap().parse::<usize>().unwrap();
-                if y == fold_pos {
-                    papers.remove(key);
-                } else if y > fold_pos {
-                    let new_y = 2 * fold_pos - y;
-                    papers.insert(format!("{},{}", x, new_y), true);
-                    papers.remove(key);
-                }
-            }
-        }
-    }
-    let mut output: Vec<Vec<bool>> = Vec::with_capacity(50);
-
-    for _ in 0..50 {
+    for _ in 0..6 {
         output.push(vec![
             false, false, false, false, false, false, false, false, false, false, false, false,
             false, false, false, false, false, false, false, false, false, false, false, false,
@@ -140,7 +95,7 @@ fn solve_part_2(contents: String) {
     for row in output {
         for col in row {
             if col {
-                print!("*");
+                print!("#");
             } else {
                 print!(" ");
             }
@@ -156,10 +111,5 @@ mod tests {
     #[test]
     fn part_1() {
         assert_eq!(solve_part_1(aoc::sample()), 17);
-    }
-
-    #[test]
-    fn part_2() {
-        assert_eq!(solve_part_2(aoc::sample()), 0);
     }
 }
