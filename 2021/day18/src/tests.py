8d4bc549fb3edec6f164f2f1accb124002903e33
Tyler Turk <tylerturk@fb.com>
2021-12-29 12:37:52 -0600

    Add the tests

--- 2021/day18/src/tests.py
+++ 2021/day18/src/tests.py
@@ -134,8 +134,48 @@ class TestSnailFish(unittest.TestCase):
         sf = SnailFishNumber.from_list("[[1,2],[[3,4],5]]")
         self.assertEqual(sf.determine_magnitude(), 143)
 
+    def test_magnitude_4(self):
+        sf1 = SnailFishNumber.from_list("[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]")
+        sf2 = SnailFishNumber.from_list("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]")
+        sf3 = SnailFishNumber(sf1, sf2)
+        sf3.reduce()
+        self.assertEqual(sf3.determine_magnitude(), 3993)
+
+    def test_magnitude_5(self):
+        sf1 = SnailFishNumber.from_list("[[[7,9],[7,8]],[[8,3],8]]")
+        sf2 = SnailFishNumber.from_list("[[[7,7],[7,7]],[[6,6],[6,6]]]")
+        sf3 = SnailFishNumber(sf1, sf2)
+        sf3.reduce()
+        self.assertEqual(sf3.determine_magnitude(), 5)
+
     def complex_magnitude(self):
         sf = SnailFishNumber.from_list("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")
         self.assertEqual(sf.determine_magnitude(), 3488)
+
+    def test_max_magnitude(self):
+        entries = [
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+            "[[[[5,4],[7,7]],8],[[8,3],8]]",
+            "[[9,3],[[9,9],[6,[4,9]]]]",
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+        ]
+        entries = [SnailFishNumber.from_list(x) for x in entries]
+        max_magnitude = 0
+        for ind_1, num_1 in enumerate(entries):
+            for ind_2, num_2 in enumerate(entries):
+                if ind_1 == ind_2:
+                    continue
+                sf = SnailFishNumber(num_1, num_2)
+                sf.reduce()
+                mag = sf.determine_magnitude()
+                if mag > max_magnitude:
+                    max_magnitude = mag
+        self.assertEqual(max_magnitude, 3993)
 
 unittest.main()
